3b6d130c56559952d902f67a438f6dc73bd2df97
Franck <fkdiomande@gmail.com>
2018-01-08 22:14:01 +0100

    Throw error if component not exists

--- skyflow.sh
+++ skyflow.sh
@@ -1,9 +1,45 @@
 #! /bin/sh
 
 if [ "$USER" == "root" ]; then
-    echo -e "\033[0;31mSkyflow error: Run without 'root' user.\033[0m"
+    printf "\033[0;31mSkyflow error: Run without 'root' user.\033[0m"
     exit 1
 fi
+
+function skyflowInit()
+{
+    case $1 in
+        "-f")
+            if [ -d $HOME/.skyflow ]; then
+                rm -rf $HOME/.skyflow
+            fi
+        ;;
+    esac
+
+    # Create skyflow directory for current user
+    if [ ! -d $HOME/.skyflow ]; then
+        mkdir $HOME/.skyflow
+    fi
+
+    if [ ! -d $HOME/.skyflow/component ]; then
+        mkdir $HOME/.skyflow/component
+    fi
+
+    if [ ! -f $HOME/.skyflow/doc.ini ]; then
+        curl -s $SKYFLOW_GITHUB_CONTENT/doc.ini -o $HOME/.skyflow/doc.ini
+    fi
+
+    if [ ! -f $HOME/.skyflow/helper.sh ]; then
+        curl -s $SKYFLOW_GITHUB_CONTENT/helper.sh -o $HOME/.skyflow/helper.sh
+    fi
+
+    if [ ! -f $HOME/.skyflow/component.txt ]; then
+        curl -s $SKYFLOW_GITHUB_CONTENT/component.txt -o $HOME/.skyflow/component.txt
+    fi
+
+}
+
+skyflowInit
+
 
 #source ./helper.sh
 source $HOME/.skyflow/helper.sh
@@ -12,51 +48,8 @@ author="Skyflow Team - Franck Diomandé <fkdiomande@gmail.com>"
 versionMessage="Skyflow CLI version $SKYFLOW_VERSION"
 docFile="$SKYFLOW_DIR/doc.ini"
 
-function skyflowInit()
-{
-    case $1 in
-        "-f")
-            if [ -d $SKYFLOW_DIR ]; then
-                rm -rf $SKYFLOW_DIR
-            fi
-        ;;
-    esac
-
-    # Create skyflow directory for current user
-    if [ ! -d $SKYFLOW_DIR ]; then
-        mkdir $SKYFLOW_DIR
-    fi
-
-    if [ ! -d $SKYFLOW_DIR/component ]; then
-        mkdir $SKYFLOW_DIR/component
-    fi
-
-    if [ ! -f $SKYFLOW_DIR/doc.ini ]; then
-        curl -s $SKYFLOW_GITHUB_CONTENT/doc.ini -o $SKYFLOW_DIR/doc.ini
-    fi
-
-    if [ ! -f $SKYFLOW_DIR/helper.sh ]; then
-        curl -s $SKYFLOW_GITHUB_CONTENT/helper.sh -o $SKYFLOW_DIR/helper.sh
-    fi
-
-    if [ ! -f $SKYFLOW_DIR/component.txt ]; then
-        curl -s $SKYFLOW_GITHUB_CONTENT/component.txt -o $SKYFLOW_DIR/component.txt
-    fi
-
-}
-
-skyflowInit
-
 function skyflowInstall()
 {
-#    componentCacheDir=$SKYFLOW_CACHE_DIR/component/$1
-
-    # Check if component exists
-#    if [ ! -d $componentCacheDir ] || test -z $1; then
-#    	skyflowHelperPrintError "$1 component not found"
-#    	exit 1
-#	fi
-
 	if [ ! grep -Fxq "$1" $SKYFLOW_DIR/component.txt ]; then
         skyflowHelperPrintError "$1 component not found"
     	exit 1
@@ -72,7 +65,7 @@ function skyflowInstall()
 #	cp -R $componentCacheDir $SKYFLOW_DIR/component/$1
 
     # Install binary
-    sudo cp $SKYFLOW_CACHE_DIR/bin/skyflow-$1.sh /usr/local/bin/skyflow-$1
+    curl -s $SKYFLOW_GITHUB_CONTENT/bin/skyflow-$1.sh -o /usr/local/bin/skyflow-$1
     sudo chmod +x /usr/local/bin/skyflow-$1
 
     skyflowHelperPrintSuccess "$1 component was successfully installed! Now you can use \033[4;94mskyflow-$1\033[0;92m CLI"
