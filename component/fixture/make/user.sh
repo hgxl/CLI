04f7eaeac02a9b215268d42623e3f8db6c3b00af
Franck <fkdiomande@gmail.com>
2018-01-13 14:41:30 +0100

    Add make for company fixture

--- component/fixture/make/user.sh
+++ component/fixture/make/user.sh
@@ -3,11 +3,10 @@
 source $HOME/.skyflow/helper.sh
 export SKYFLOW_FIXTURE_DIR=$SKYFLOW_DIR/component/fixture
 
-mkdir -p ${SKYFLOW_FIXTURE_DIR}/data/company
+mkdir -p ${SKYFLOW_FIXTURE_DIR}/data/user
 
-skyflowHelperPullFromRemote "component/fixture/data/company/country.txt" ${SKYFLOW_FIXTURE_DIR}/data/company/country.txt
-skyflowHelperPullFromRemote "component/fixture/data/company/description.txt" ${SKYFLOW_FIXTURE_DIR}/data/company/description.txt
-skyflowHelperPullFromRemote "component/fixture/data/company/logo.txt" ${SKYFLOW_FIXTURE_DIR}/data/company/logo.txt
-skyflowHelperPullFromRemote "component/fixture/data/company/name.txt" ${SKYFLOW_FIXTURE_DIR}/data/company/name.txt
-skyflowHelperPullFromRemote "component/fixture/data/company/sector.txt" ${SKYFLOW_FIXTURE_DIR}/data/company/sector.txt
-skyflowHelperPullFromRemote "component/fixture/data/company/turnover.txt" ${SKYFLOW_FIXTURE_DIR}/data/company/turnover.txt
+skyflowHelperPullFromRemote "component/fixture/data/user/avatar.txt" ${SKYFLOW_FIXTURE_DIR}/data/user/avatar.txt
+skyflowHelperPullFromRemote "component/fixture/data/user/email.txt" ${SKYFLOW_FIXTURE_DIR}/data/user/email.txt
+skyflowHelperPullFromRemote "component/fixture/data/user/firstname.txt" ${SKYFLOW_FIXTURE_DIR}/data/user/firstname.txt
+skyflowHelperPullFromRemote "component/fixture/data/user/lastname.txt" ${SKYFLOW_FIXTURE_DIR}/data/user/lastname.txt
+skyflowHelperPullFromRemote "component/fixture/data/user/password.txt" ${SKYFLOW_FIXTURE_DIR}/data/user/password.txt
